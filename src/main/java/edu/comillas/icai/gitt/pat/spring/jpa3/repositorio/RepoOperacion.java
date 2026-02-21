6fa3003a641da6979f56048a53f15465079fe5a4
Atilano Fernandez Pacheco <atilanof@hotmail.com>
2026-02-21 12:24:02 +0100

    v0.3 Clase del 20/02

--- src/main/java/edu/comillas/icai/gitt/pat/spring/jpa3/repositorio/RepoOperacion.java
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/jpa3/repositorio/RepoOperacion.java
@@ -1,12 +1,11 @@
 package edu.comillas.icai.gitt.pat.spring.jpa3.repositorio;
 
 
-import edu.comillas.icai.gitt.pat.spring.jpa3.entity.Contador;
 import edu.comillas.icai.gitt.pat.spring.jpa3.entity.Operacion;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface RepoOperacion extends CrudRepository<Operacion, Contador> {
+public interface RepoOperacion extends CrudRepository<Operacion, Long> {
     List<Operacion> findByContador(Contador contador);
 }
